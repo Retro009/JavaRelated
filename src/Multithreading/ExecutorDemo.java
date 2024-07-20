fd421fb03672076f0ef4c85d12c0cc3daf1533df
Jeeshan Shaikh <jeeshannick@gmail.com>
2024-07-20 13:50:49 +0530

    ExecutorDemo Removed

--- src/Multithreading/ExecutorDemo.java
+++
@@ -1,9 +0,0 @@
-package Multithreading;
-
-public class ExecutorDemo {
-    public static void main(String[] args) {
-        Thread t1 = new Thread(()-> System.out.println("Child Thread: "+Thread.currentThread()));
-        t1.start();
-        System.out.println("Main Thread: "+Thread.currentThread());
-    }
-}
